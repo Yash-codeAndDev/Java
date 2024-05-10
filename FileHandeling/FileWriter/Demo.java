1e12a21acd5fa221194df7d7f4351bfc147dff60
Yash Khati <160757711+Yash-codeAndDev@users.noreply.github.com>
2024-05-10 02:57:46 +0000

    File Writer write methods and flush

--- FileHandeling/FileWriter/Demo.java
+++ FileHandeling/FileWriter/Demo.java
@@ -7,6 +7,23 @@ public class Demo {
         File f = new File("abc.txt");
 
         FileWriter fw = new FileWriter(f);
+        
+        /*
+        *   write(int c)
+            writes the character corresponding to the Unicode value to the file
+        
+        *   write(char[] ch) 
+            writes entire array to file
+        
+        *   write(char[] ch, int start, int len)
+            writes a portion of the character array starting from index start (inclusive) with a length of len characters
+        
+        *   write(String str)
+            writes entire string to file
+        
+        *   write(String str,int start,int len)
+            writes a portion of the string starting from index start (inclusive) with a length of len characters.
+        */
 
         fw.write("Learning Java\nFile Handeling in java\n");
           
