b8171db2a6344321cbab3e028126827a0049519b
Yash Khati <160757711+Yash-codeAndDev@users.noreply.github.com>
2024-05-12 04:51:33 +0000

    notes

--- FileHandeling/Serialization/Demo.java
+++ FileHandeling/Serialization/Demo.java
@@ -24,12 +24,13 @@ public class Demo{
         // Serialization
         FileOutputStream fos = new FileOutputStream("abc.ser");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
-        oos.writeObject(dobj);
+        oos.writeObject(dobj); // writing object to file 
     
+        // Deserialization
         FileInputStream fis = new FileInputStream("abc.ser");
         ObjectInputStream ois = new ObjectInputStream(fis);
-        
-        Dog d2 = (Dog)ois.readObject();
+        Dog d2 = (Dog)ois.readObject(); // reading object form file
+
         System.out.println(d2.Breed+" "+d2.name+" "+d2.age);
     }   
 }
