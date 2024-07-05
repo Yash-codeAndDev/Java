f6b9a1569a0bc5d7b63053eb87f32cc3907290f5
Yash Khati <160757711+Yash-codeAndDev@users.noreply.github.com>
2024-07-05 05:24:27 +0000

    Method Overriding Exception Rules

---
+++ MethodOverriding/ExceptionsOnMethod/Demo2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class P2{
+    public void m1() throws ArithmeticException{
+
+    }
+}
+
+class C2 extends P2 
+{
+    public void m1() throws NullPointerException{
+
+    }
+}
+
+/*
+class C2 extends P2 
+{
+    public void m1() throws IOException{
+
+    }
+}
+*/
+
