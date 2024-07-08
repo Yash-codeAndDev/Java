066ab0823c1c5fd236d6ca485bb5398eb02979a5
Yash Khati <160757711+Yash-codeAndDev@users.noreply.github.com>
2024-07-08 14:42:18 +0000

    Static Flow

---
+++ StaticVariablesAndMethods/StaticFlow/Demo.java
@@ -0,0 +1,23 @@
+public class Demo {
+
+    static int i = 10;
+    static{
+        m1();
+        System.out.println("First Static Block");
+    }
+
+    public static void main(String[] args) {
+        m1();
+        System.out.println("Main Method");
+    }
+
+    public static void m1() 
+    {
+        System.out.println("j = "+j);   
+    }
+    static{
+        System.out.println("Second Static Block");
+    }
+    static int j = 20;
+    
+}
